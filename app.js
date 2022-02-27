147aaf0cabc070a5423029fc0110392124b3b117
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-27 14:11:27 -0600

    Agrega la funcion de comentar las respuestas

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const template = document.getElementById("template");
-const fragment = document.createDocumentFragment();
 const form = document.querySelector(".form");
 const commentsContainer = document.getElementById("comments-container");
-let allComments = [];
+let allComments = [{
+  uid: crypto.randomUUID(),
+  message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates modi nulla aspernatur ratione, dignissimos fuga, itaque voluptatem fugit facilis beatae sapiente eligendi alias dolorum minima. Animi ducimus vero eum iusto!",
+  likes: 7,
+  replies: [],
+}];
+let parentComment;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,28 +14,68 @@ form.addEventListener("submit", (e) => {
   form.reset();
 });
 
-const saveData = (msg) => {
+const saveData = (msg, uid = "") => {
   const comment = {
     uid: crypto.randomUUID(),
     message: msg,
     likes: 0,
     replies: [],
   };
-  allComments = [...allComments, comment];
-  console.log(allComments);
-  paintComment(comment);
+  if (uid === "") {
+    allComments = [...allComments, comment];
+    paintComment(comment);
+  } else {
+    commentFilter = allComments.filter((item) => item.uid === uid);
+    commentFilter.replies = comment;
+    paintComment(commentFilter.replies);
+  }
 };
 
 const paintComment = (comment) => {
   //template
-  const template = document.getElementById("template").content;
-  const templateClone = template.cloneNode(true);
-  const fragment = document.createDocumentFragment();
-
+  const templateComment = document.getElementById("templateComment").content;
+  const templateClone = templateComment.cloneNode(true);
+  const templateReply = templateComment .cloneNode(true).querySelector(".replyCommentContainer");
+  // Comment content
+  templateClone.querySelector(".main-comment").dataset.uid = comment.uid;
   templateClone.querySelector(".comment__text").textContent = comment.message;
   templateClone.querySelector(".aside__count").textContent = comment.likes;
-  templateClone.querySelector(".reply").dataset.uid = comment.uid;
-  templateClone.querySelector(".reply__img").dataset.uid = comment.uid;
-  fragment.appendChild(templateClone);
-  commentsContainer.appendChild(fragment);
+  // Si es un comentario de respuesta:
+  if (parentComment) {
+    templateReply.appendChild(templateClone);
+    parentComment.appendChild(templateReply);
+    parentComment = false;
+  } else {
+    commentsContainer.appendChild(templateClone);
+  }
 };
+
+// Delegacion de eventos ("Click")
+commentsContainer.addEventListener("click", (evt) => {
+  const target = evt.target;
+  const templateForm = document.getElementById("templateForm").content;
+  const formReply = templateForm.cloneNode(true).querySelector(".form");
+  // Si target es reply
+  if (target.className === "reply" || target.className === "reply__img") {
+    target.className === "reply"
+      ? commentContainer = target.parentElement.parentElement.parentElement
+      : commentContainer = target.parentElement.parentElement.parentElement.parentElement;
+    // Inserta el formulario 
+    insertAfter(formReply, commentContainer.parentElement);
+  }
+  // Si target es submit Form
+  if (target.classList.contains("form__btn--reply")) {
+    evt.preventDefault();
+    const inputValue = target.previousElementSibling.value;
+    parentComment = target.parentElement.previousElementSibling;
+    saveData(inputValue, parentComment.dataset.uid);
+
+    // Elimina el formReply
+    target.parentNode.parentNode.removeChild(target.parentNode);
+  }
+});
+
+// Insertar formReply
+const insertAfter = (newNode, parentNode) => parentNode.after(newNode);
+
+paintComment(allComments[0])
